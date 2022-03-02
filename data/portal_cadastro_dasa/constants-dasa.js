876b4bfd69af67a383e887bd73bc761343694741
Rene <rene.souza@fcamara.com.br>
2022-03-02 16:33:14 -0300

    ajuste

---
+++ data/portal_cadastro_dasa/constants-dasa.js
@@ -0,0 +1,4 @@
+module.exports = {
+  exames: [14275, 11364],
+  marcas: [19],
+};
---
+++ data/portal_cadastro_dasa/db-dasa.js
@@ -0,0 +1,9 @@
+module.exports = {
+  search_exames: require("./exames/search_exames.json"),
+  detalhe_exames: require("./exames/detalhe_exames.json"),
+  marcas: require("./exames/marcas.json"),
+  unidades: require("./exames/unidades.json"),
+  preparos: require("./exames/preparos.json"),
+  integracoes: require("./exames/integracoes.json"),
+  list_combos: require("./cadastro/list_combos.json"),
+};
